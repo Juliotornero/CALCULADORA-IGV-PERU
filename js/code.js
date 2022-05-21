0406f395f59b5582c0fdedf9fb3a3a4664da7c46
Julio Tornero <juliotornero8@gmail.com>
2022-05-21 11:06:46 -0500

    Terminado

---
+++ js/code.js
@@ -0,0 +1,14 @@
+
+const input_subtotal = document.querySelector('.subtotal');
+const input_igv = document.querySelector('.igv');
+const input_total = document.querySelector('.total');
+
+input_total.addEventListener('keyup', ()=>{
+    input_subtotal.value = input_total.value/1.18;
+    input_igv.value = parseFloat(input_total.value) - parseFloat(input_subtotal.value);
+});
+
+input_subtotal.addEventListener('keyup', ()=>{
+    input_igv.value = input_subtotal.value*0.18;
+    input_total.value = parseFloat(input_subtotal.value) + parseFloat(input_igv.value);
+});
