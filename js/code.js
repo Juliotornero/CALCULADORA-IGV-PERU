3d0a654da5dea87e3faf026ac20abf54d0f915f7
Juliotornero <76627913+Juliotornero@users.noreply.github.com>
2022-10-09 14:28:40 -0500

    Actualizacion igv 10%

--- js/code.js
+++ js/code.js
@@ -4,11 +4,11 @@ const input_igv = document.querySelector('.igv');
 const input_total = document.querySelector('.total');
 
 input_total.addEventListener('keyup', ()=>{
-    input_subtotal.value = input_total.value/1.18;
+    input_subtotal.value = input_total.value/1.10;
     input_igv.value = parseFloat(input_total.value) - parseFloat(input_subtotal.value);
 });
 
 input_subtotal.addEventListener('keyup', ()=>{
-    input_igv.value = input_subtotal.value*0.18;
+    input_igv.value = input_subtotal.value*0.10;
     input_total.value = parseFloat(input_subtotal.value) + parseFloat(input_igv.value);
 });
